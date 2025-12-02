a6aa28d7c7cb0a6fd9ec5fe9665ab010fc1eee16
Mark Chadwick <31071714+astorguy@users.noreply.github.com>
2025-12-02 02:26:15 +0000

    update devcontainer.json

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,18 +2,21 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# uv sync --dev
+
+
 # Update package list and install ngspice
-echo "--- Installing ngspice ---"
-sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ngspice
+# echo "--- Installing ngspice ---"
+# sudo apt-get update
+# sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ngspice
 
 # Install Python packages
 echo "--- Installing Python packages ---"
 # Note: The 'pip install' command is run by the 'remoteUser' (vscode),
 # so the '--user' flag is appropriate here.
-pip install --user \
-    ipykernel==7.1.0 \
-    matplotlib==3.10.7 \
-    numpy==2.3.5 \
-    py4spice==0.1.0
+# pip install --user \
+#     ipykernel==7.1.0 \
+#     matplotlib==3.10.7 \
+#     numpy==2.3.5 \
+#     py4spice==0.1.0
 
